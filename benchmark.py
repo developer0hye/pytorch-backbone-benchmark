d4696c3657f99ed40c2bb68a17ce012c10d83fa9
Yonghye Kwon <developer.0hye@gmail.com>
2021-05-25 19:09:10 +0900

    upload

--- benchmark.py
+++ benchmark.py
@@ -1,20 +1,17 @@
-import resnet
-import dla
-import hardnet
 import torch
 import timm
 import time
-import cspdarknet
 
-def benchmark(model, input_size=(608, 608), times=500):
+
+def benchmark(model, batch_size=1, input_size=[608, 608], times=100):
     with torch.no_grad():
         model.eval()
         
-        input = torch.rand(1,3,input_size[0],input_size[1]).cuda()
-        model(input) #warmup
-        model(input) #warmup
+        input = torch.rand(batch_size, 3, input_size[0], input_size[1]).cuda()
+        for _ in range(10):
+            model(input) #gpu warmup
+            
         avg_time = 0
-
         for i in range(0, times):
             input = torch.rand(1,3,input_size[0],input_size[1]).cuda()
             torch.cuda.synchronize()
@@ -31,9 +28,13 @@ def benchmark(model, input_size=(608, 608), times=500):
 torch.backends.cudnn.benchmark = True
 
 model_names = timm.list_models(pretrained=False)
-with open("bench_x500.txt", "w") as f:
+with open("bench.txt", "w") as f:
     for model_name in model_names:
-        model = timm.create_model(model_name, pretrained=False).cuda()
-        model.eval()
-        print(model_name, " ", benchmark(model))
-        f.write(model_name+" "+str(benchmark(model))+"\n")
+        try:
+            model = timm.create_model(model_name, pretrained=False).cuda()
+            model.eval()
+            print(model_name, " ", benchmark(model))
+            f.write(model_name+" "+str(benchmark(model))+"\n")
+        except:
+            continue
+        
--- read_bench.py
+++ read_bench.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 models = []
 execution_times = []
@@ -9,35 +10,30 @@ with open("bench.txt", "r") as f:
         model, execution_time = line.split(" ")
         
         models.append(model)
-        execution_times.append(float(execution_time))
+        execution_times.append(np.round(float(execution_time), 4))
 
-df = pd.DataFrame([], columns=["model", "execution time"])
+execution_times = np.array(execution_times)
+
+df = pd.DataFrame([], columns=["model", "execution time(sec)"])
 
 df["model"] = models
-df["execution time"] = execution_times
+df["execution time(sec)"] = execution_times
+df["fps"] = np.floor(1/execution_times)
 
-df = df[df["execution time"] < 0.016]
-
-print(df)
-
-total_df = pd.read_csv("results-imagenet.csv")
-total_df["execution time"] = 0
+total_df = pd.read_csv("pytorch-image-models/results/results-imagenet.csv")
+total_df["execution time(sec)"] = 0
+total_df["fps"] = 0
 
 for index, row in df.iterrows():
-    total_df.loc[total_df["model"] == row["model"], "execution time"] = row["execution time"]
+    total_df.loc[total_df["model"] == row["model"], "execution time(sec)"] = row["execution time(sec)"]
+    total_df.loc[total_df["model"] == row["model"], "fps"] = row["fps"]
 
-total_df = total_df[total_df["execution time"] != 0]
-
-print(total_df.loc[total_df["model"] == "cspdarknet53", "top1"])
-
-total_df = total_df[total_df["top1"] >= 74]
+total_df = total_df[total_df["execution time(sec)"] != 0]
+total_df = total_df.loc[:, ["model", "top1", "param_count", "execution time(sec)", "fps"]]
+total_df = total_df.sort_values(by=['execution time(sec)'], axis=0)
 total_df.reset_index(drop=True, inplace=True)
 
-total_df
-print(total_df.loc[:, ["model", "top1", "param_count", "img_size", "execution time"]])
-
-total_df = total_df.loc[:, ["model", "top1", "param_count", "img_size", "execution time"]]
-total_df = total_df.sort_values(by=['execution time'], axis=0)
+total_df.to_csv("benchmark.csv")
 
 markdown_table = total_df.to_markdown()
 print(markdown_table)
